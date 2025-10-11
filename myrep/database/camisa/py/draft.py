e83734fbeadddf9d00fdbb6df1972cb59c512228
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-11 11:15:00 -0300

    atividade camisa 30 min

--- myrep/database/camisa/py/draft.py
+++ myrep/database/camisa/py/draft.py
@@ -7,3 +7,17 @@ class Camisa:
     
     def setTamanho(self, valor: str):
         tamanhosValidos = ["PP", "P", "M", "G", "GG", "XG"]
+
+        if valor not in tamanhosValidos:
+            print("fail tamanho invalido")
+            return
+        
+        self.__tamanho = valor
+
+camisa = Camisa()
+while camisa.getTamanho() == "":
+    print("Digite o seu tamanho de roupa")
+    tamanho = input()
+    camisa.setTamanho(tamanho)
+print(f"Parabens, você comprou uma roupa tamanho", camisa.getTamanho())
+
