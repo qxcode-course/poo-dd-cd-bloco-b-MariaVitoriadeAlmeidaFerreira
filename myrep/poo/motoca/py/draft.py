57123c96dcb8b68796939f82ba5346d7f52cbe1c
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-21 20:20:50 -0300

    fazendo def remove 20 min

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -15,9 +15,25 @@ class Person:
     
 class motinha:
     def __init__(self, power:int = 1):
-        self.__person: Person | None 
+        self.__assento: Person | None = None
         self.__power: int = power
         self.__time: int = 0
 
+    def inserirPessoa(self, person: Person) -> bool:
+        if self.__assento == None:
+            self.__assento = person
+            return True
+        else:
+            print("fail: busy motorcycle")
+            return False
+        
+    def remover(self) -> Person | None:
+        if self.__assento != None:
+            aux = self.__assento 
+            self.__assento = None
+            return aux
+        else:
+            print("fail: empty motorcycle")
+            return None
 
         
