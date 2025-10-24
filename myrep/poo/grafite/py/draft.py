f8978d820ad0221af67dcd43f96ffa25380c6c36
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-24 09:46:57 -0300

    grafite: fazendo def insert 30 min

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -15,8 +15,34 @@ class Lead:
     
     def setSize(self, size:int):
         self.__size = size
-
-    def usageper
+    
+    #sena pelo o amor de Deus que nome é esse
+    def usagepersheet(self) -> int:
+        if self.__thickness == 'HB':
+            return 1
+        elif self.__thickness == '2B':
+            return 2
+        elif self.__thickness == "4B":
+            return 4
+        elif self.__thickness == "6B":
+            return 6
+        return 0
     
     def __str__(self):
         return f"{self.__thickness}:{self.__hardness}: {self.__size}"
+    
+class Pencil:
+    def __init__(self, thickness: float):
+        self.__thickness = thickness
+        self.__tip: Lead | None
+
+    def HasGrafite(self) -> bool:
+        if self.__tip != None:
+            return True
+
+    def insert(self, lead: Lead) -> bool:
+        if self.HasGrafite():
+            print("")
+            return False
+        self.__tip = lead
+        return True
