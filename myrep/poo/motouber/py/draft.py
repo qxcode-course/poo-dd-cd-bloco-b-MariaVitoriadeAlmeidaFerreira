c94a4a22fc877e267daad78275e14fa48fb04a58
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-26 14:34:42 -0300

    iniciando atividade motouber

---
+++ myrep/poo/motouber/py/draft.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    def __init__(self, nome:str, dinheiro:int):
+        self.__nome: str = nome
+        self.__dinheiro: int = dinheiro
+
+    def getNome(self) -> str:
+        return self.__nome
+    
+    def getDinheiro(self) -> int:
+        return self.__dinheiro
+    
+class Moto:
+    def __init__(self, )
+    
