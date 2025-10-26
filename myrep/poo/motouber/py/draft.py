1f1049ea2ccfb28bd8fa5a88220124643070bdf4
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-26 15:39:45 -0300

    continuando a motouber 60 min

--- myrep/poo/motouber/py/draft.py
+++ myrep/poo/motouber/py/draft.py
@@ -9,6 +9,39 @@ class Pessoa:
     def getDinheiro(self) -> int:
         return self.__dinheiro
     
+    def __str__(self) -> str:
+        return f"{self.__nome}:{self.__dinheiro}"
+    
 class Moto:
-    def __init__(self, )
+    def __init__(self):
+        self.__custo: int = 0
+        self.__motorista: Pessoa | None = None
+        self.__passageiro: Pessoa | None = None
+
+    def Driver(self, motorista: Pessoa) -> None:
+        if self.__motorista == None:
+            self.__motorista = motorista
+        
+    def
+
+    
+    def __str__(self) -> str:
+        return f"Cost: {self.__custo}, Driver: {self.__motorista}, Passenger: {self.__passageiro}"
+
+def main():
+    moto = Moto()
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str]  = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(moto)
+        elif args[0] == "setDriver":
+            motorista = Pessoa(args[1], int(args[2]))
+            moto.Driver(motorista)
+
+main()
+
     
