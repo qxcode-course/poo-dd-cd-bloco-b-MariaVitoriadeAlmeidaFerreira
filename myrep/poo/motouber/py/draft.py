556916bf23884b429820678721b58a8cdb08abf9
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-10-26 17:12:14 -0300

    motouber 120 min

--- myrep/poo/motouber/py/draft.py
+++ myrep/poo/motouber/py/draft.py
@@ -12,6 +12,15 @@ class Pessoa:
     def __str__(self) -> str:
         return f"{self.__nome}:{self.__dinheiro}"
     
+    def receber(self, dinheiro:int) -> None:
+        self.__dinheiro += dinheiro
+
+    def pagar(self, dinheiro:int) -> None:
+        self.__dinheiro -= dinheiro
+    
+    def setDinheiro(self, dinheiro:int) -> None:
+        self.__dinheiro = dinheiro
+    
 class Moto:
     def __init__(self):
         self.__custo: int = 0
@@ -22,9 +31,26 @@ class Moto:
         if self.__motorista == None:
             self.__motorista = motorista
         
-    def
+    def Pass(self, passageiro: Pessoa) -> None:
+        if self.__passageiro == None:
+            self.__passageiro = passageiro
 
+    def Drive(self, km: int) -> None:
+        self.__custo += km
     
+    def Left(self) -> Pessoa | None:
+        if self.__passageiro != None:
+            aux = self.__passageiro
+            self.__passageiro.pagar(self.__custo)
+            if self.__passageiro.getDinheiro() < 0:
+                self.__passageiro.setDinheiro(0)
+                print("fail: Passenger does not have enough money")
+            self.__motorista.receber(self.__custo)
+            self.__custo = 0
+            self.__passageiro = None
+            return aux
+        
+        
     def __str__(self) -> str:
         return f"Cost: {self.__custo}, Driver: {self.__motorista}, Passenger: {self.__passageiro}"
 
@@ -41,6 +67,18 @@ def main():
         elif args[0] == "setDriver":
             motorista = Pessoa(args[1], int(args[2]))
             moto.Driver(motorista)
+        elif args[0] == "setPass":
+            passageiro = Pessoa(args[1], int(args[2]))
+            moto.Pass(passageiro)
+        elif args[0] == "drive":
+            km = int(args[1])
+            moto.Drive(km)
+        elif args[0] == "leavePass":
+            pessoa = moto.Left()  
+            print(f"{pessoa} left")
+
+
+
 
 main()
 
